9dadc411c49e509090dacef0b26f800c3794ce2f
Vladimir Simin <vladimir.simin@gmail.com>
2021-11-12 18:35:14 +0100

    task 1 - setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AppCustomers from './components/AppCustomers';
+import AppProducts from './components/AppProducts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/customers'>Customers</Link>
+            </li>
+            <li>
+              <Link to='/products'>Products</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/customers'>
+            <AppCustomers />
+          </Route>
+          <Route path='/products'>
+            <AppProducts />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
